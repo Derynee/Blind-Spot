9bdbb7a09fa9a699c73bb6e2f82277ff277098a8
Arcelia Denisse <alopez92@alumnos.uaq.mx>
2022-12-14 21:30:09 -0600

    Destrozo 1
    Ya no sirve el sistema :(

---
+++ BLIND SPOT/Assets/Scripts/Buttons/RightButtonUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RightButtonUI : MonoBehaviour
+{
+    
+    /* public void GoToRightState()
+    {
+        WallMain.state = State.nextStates[0];
+        Debug.Log("Clicked Right");
+        
+    }
+
+    public void GoToLeftState() 
+    {
+        WallMain.state = State.nextStates[1];
+        Debug.Log("Clicked Left");
+    } 
+     */
+}
